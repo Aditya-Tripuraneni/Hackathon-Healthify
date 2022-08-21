c17ea7804ba546e49635e522e479e8dfd928b0c8
Charmi Kadi <68164274+charmikadi@users.noreply.github.com>
2022-08-21 02:17:52 -0400

    Update landing.js

--- client/scripts/landing.js
+++ client/scripts/landing.js
@@ -1,8 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  const firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyA9XE1UJ4l5-6hCyinlfbKAwjrnxQVcVQo",
     authDomain: "healthify-de8b2.firebaseapp.com",
     projectId: "healthify-de8b2",
@@ -10,104 +6,135 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase
     messagingSenderId: "995622981984",
     appId: "1:995622981984:web:26a574d7d91d25b755ce05"
   };
-
-  const app = initializeApp(firebaseConfig);
-
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // Initialize variables
   const auth = firebase.auth()
   const database = firebase.database()
   
+  // Set up our register function
   function register () {
-  // Get all our input fields
-  email = document.getElementById('email').value
-  password = document.getElementById('password').value
-  full_name = document.getElementById('full_name').value
-    
-  if (validate_email(email) == false || validate_password(password) == false) {
-    alert('Email or Password is incorrect')
-    return
-  }
-    
-  auth.createUserWithEmailAndPassword(email, password)
-    .then(function() {
-    
-    	var user = auth.currentUser
-      alert('User Created!')
-    
-      var database_ref = database.ref()
-      
-      var user_data = {
-        email : email, 
-        full_name : full_name
-        last_login : Date.now()  
-      }
-        
-      database_ref.child('users/' + user.uid).update(user_data)
-
-  })
-    .catch(function(error)) {
-           var error_code = error.code
-           var error_message = error.message
-           
-           alert(error_message)
-
-  function login () {
-      email = document.getElementById('email').value
-      password = document.getElementById('password').value
-    
-      if (validate_email(email) == false || validate_password(password) == false) {
-        alert('Email or Password is incorrect')
-        return
-      }
-      
-      auth.signInWithEmailAndPassword(email, password)
-      .then(function() {
-        var user = auth.currentUser
-        
-        var database_ref = database.ref()
-        
-        var user_data = {
-          last_login : Date.now()
-        }
-        
-        database_ref.child('users/' + user.uid).update(user_data)
-        
-        alert('User Logged In!')
-
-      })
-      .catch(function(error) {
-        var error_code = error.code
-        var error_message = error.message
-
-        
-      })
-  }
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+    full_name = document.getElementById('full_name').value
+  
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is Outta Line!!')
+      return
+      // Don't continue running the code
+    }
+    if (validate_field(full_name) == false) {
+      alert('One or More Extra Fields is Outta Line!!')
+      return
+    }
    
+    // Move on with Auth
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        email : email,
+        full_name : full_name,
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).set(user_data)
+  
+      // DOne
+      alert('User Created!!')
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
+  
+  // Set up our login function
+  function login () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+  
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is Outta Line!!')
+      return
+      // Don't continue running the code
+    }
+  
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).update(user_data)
+  
+      // DOne
+      alert('User Logged In!!')
+  
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
+  
+  
+  
+  
+  // Validate Functions
   function validate_email(email) {
     expression = /^[^@]+@\w+(\.\w+)+\w$/
-  if (expression.test(email) == true) {
-    return true
-  } else {
-    return false
+    if (expression.test(email) == true) {
+      // Email is good
+      return true
+    } else {
+      // Email is not good
+      return false
+    }
   }
-}
-    
+  
   function validate_password(password) {
     // Firebase only accepts lengths greater than 6
     if (password < 6) {
       return false
     } else {
       return true
+    }
   }
-}
-    
+  
   function validate_field(field) {
     if (field == null) {
       return false
-  }
-    
-    if (field.length <= 0) {	
+    }
+  
+    if (field.length <= 0) {
       return false
     } else {
       return true
+    }
   }
-}
