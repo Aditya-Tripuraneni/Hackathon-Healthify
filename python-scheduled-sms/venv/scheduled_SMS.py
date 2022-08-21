8c164885815029bba818ac40ea93207ce746614b
Tirth Nagar <tirthnagar.n1@gmail.com>
2022-08-20 20:25:31 -0400

    update

--- python-scheduled-sms/venv/scheduled_SMS.py
+++ python-scheduled-sms/venv/scheduled_SMS.py
@@ -11,14 +11,14 @@ client = Client(account_sid, auth_token)
 # schedule message to be sent 61 minutes after current time
 send_when = datetime.utcnow() + timedelta(minutes=16)
 
-# send the SMS
-messaging_service_sid = "MGeebb5010283a9b09cb887fce7e513288"
-message = client.messages.create(
-    from_=messaging_service_sid,
-    to='+16479881095',  # ← your phone number here
-    body='Friendly reminder that you have an appointment with us next week.',
-    schedule_type='fixed',
-    send_at=send_when.isoformat() + 'Z',
-)
+# # send the SMS
+# messaging_service_sid = "MGeebb5010283a9b09cb887fce7e513288"
+# message = client.messages.create(
+#     from_=messaging_service_sid,
+#     to='+16479881095',  # ← your phone number here
+#     body='Friendly reminder that you have an appointment with us next week.',
+#     schedule_type='fixed',
+#     send_at=send_when.isoformat() + 'Z',
+# )
 
 # print(message.sid)
